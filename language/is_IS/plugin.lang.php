30a6a179602875cc3e38b3bae2aa8642fd6422f8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-14 06:27:32 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['#images(id,storage_category_id) in #image_category'] = '#images(id,storage_category_id) í #image_category';
+$lang['Check database integrity'] = 'Athugaðu heilleika gagnagrunns';
+$lang['Passed'] = 'Samþykkt';
+$lang['Pemalinks'] = 'Fasttengill';
+$lang['Result'] = 'Niðurstaða';
+$lang['Test'] = 'Prófun';
+$lang['Uploadable virtual cats'] = 'Sýndarkettir sem hægt er að hlaða upp';
