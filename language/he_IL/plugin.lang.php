85e5ae92bdbc6836b35e37e9d7f8f7befd94636e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:05:01 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['#images(id,storage_category_id) in #image_category'] = '#תמונות (id,storage_category_id) ב#קטגוריית_תמונות';
+$lang['Check database integrity'] = 'בדוק את תקינות בסיס הנתונים';
+$lang['Passed'] = 'עבר';
+$lang['Pemalinks'] = 'קישורים קבועים';
+$lang['Result'] = 'תוצאה';
+$lang['Test'] = 'בדיקה';
+$lang['Uploadable virtual cats'] = 'חתולים וירטואליים הניתנים להעלאה';
