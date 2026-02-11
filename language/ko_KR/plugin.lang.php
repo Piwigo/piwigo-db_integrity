3c289a94a717cf8a6a78e12ab23d03407b95d1ad
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-11 06:58:51 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,14 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['#images(id,storage_category_id) in #image_category'] = '#image_category의 #images(id,storage_category_id)';
+$lang['Uploadable virtual cats'] = '사용자 추가를 위해 승인된 가상 카테고리';
+$lang['Passed'] = '통과됨';
+$lang['Check database integrity'] = '데이터베이스 무결성 확인';
+$lang['Pemalinks'] = '퍼머링크';
+$lang['Result'] = '결과';
+$lang['Test'] = '테스트';
